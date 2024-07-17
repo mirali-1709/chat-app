2b023f9dc2c514c23f4f45a675b07aa09dfcfc18
Mirali Chhag <58205024+mirali-1709@users.noreply.github.com>
2024-07-17 12:01:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.broadcast.emit('chat message', 'a user is connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+      });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+
+    socket.broadcast.emit('chat message', 'a user is disconnected');
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
